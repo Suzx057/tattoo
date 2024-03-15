f4c531b5d784ca54ad8945ffa44bfd383b160069
Suzx057 <gartartzz@gmail.com>
2024-03-15 22:21:42 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <header className='app-header'>
+          <h4>Just tattoo</h4>
+        </header>
+        <div className="app-grid">
+            <div>123456 123456 123456 123456 123456 123456 123456 123456 123456 123456 123456 123456 123456 123456</div>
+            <div>123456 123456 123456 123456 123456 123456 123456 123456 123456 123456 123456 123456 123456 123456</div>
+            <div>123456 123456 123456 123456 123456 123456 123456 123456 123456 123456 123456 123456 123456 123456</div>
+            <div>123456 123456 123456 123456 123456 123456 123456 123456 123456 123456 123456 123456 123456 123456</div>
+        </div>
     </div>
   );
 }
